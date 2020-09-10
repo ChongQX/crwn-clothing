cc008cd478a4b9b40551c6ab8e10a2d6e2b78a24
Qi Xiang <qixiangchong@gmail.com>
2020-09-10 14:58:27 +0800

    Updates

---
+++ precache-manifest.6ff346aeb8a989399b8693163ac02f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7952d734da146ce27764e8d490edde14",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "95036807da603e0a04e4",
+    "url": "/crwn-clothing/static/css/main.8779153e.chunk.css"
+  },
+  {
+    "revision": "ba4d4668ecd01d314a26",
+    "url": "/crwn-clothing/static/js/2.7949cf59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crwn-clothing/static/js/2.7949cf59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95036807da603e0a04e4",
+    "url": "/crwn-clothing/static/js/main.2a5feb17.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  }
+]);
