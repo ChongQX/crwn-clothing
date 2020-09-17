9604e9efb058d54da9140d720821d5b5e18166ff
Qi Xiang <qixiangchong@gmail.com>
2020-09-18 01:16:38 +0800

    added header shop & contact

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
-        <Route exact path='/crwn-clothing' component={HomePage} />
+        <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
