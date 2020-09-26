dc1f57e14b4bc0dea158c7c507552b6d13d3cea4
Qi Xiang <qixiangchong@gmail.com>
2020-09-26 12:33:47 +0800

    added firebase

--- src/App.js
+++ src/App.js
@@ -3,20 +3,47 @@ import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component';
+import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 
-function App() {
-  return (
-    <div>
-    <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+      <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './signin-and-signup.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+    <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUpPage;
