0a497c35dd4479ed828e1f38c76ce16ea4d083ed
Qi Xiang <qixiangchong@gmail.com>
2020-10-18 16:18:13 +0800

    complete checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -16,12 +16,12 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
             </div>
             <span className='name'>{name}</span>
             <span className='quantity'>
-                <div className='arrow' onClick={() => removeItem(CartItem)}>&#10094;</div>
+                <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
                 <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={() => addItem(CartItem)}>&#10095;</div>
+                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
             </span>
             <span className='price'>{price}</span>
-            <div className='remove-button' onClick={() => clearItem(CartItem)} >&#10006;</div>
+            <div className='remove-button' onClick={() => clearItem(cartItem)} >&#10006;</div>
         </div>
     )
 };
