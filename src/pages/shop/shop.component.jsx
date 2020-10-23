c5437a14ae1f5e5f3a33f1d6ba0b078b0c925dc7
Qi Xiang <qixiangchong@gmail.com>
2020-10-24 01:14:03 +0800

    moved directory and shop data to own reducer, routing to repective shop category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-import { selectCollections } from '../../redux/shop/shop.selectors';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))}
+        <Route exact path={`${match.path}`} component={CollectionOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-  });
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
@@ -6,3 +7,17 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+export const selectCollectionsPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
+//.find() return all items that are true
+//currying => fx return fx
+//code won't recompute its value when wrapped with memoize
+export const selectCollection = memoize((collectionUrlParam) =>
+    createSelector(
+        [selectCollections],
+        collections => collections[collectionUrlParam]
+    ));
